d299c2e30edd36215a0c8e1f389351e3d27653df
Carl Willimott <carlwillimott@googlemail.com>
2015-05-03 23:52:51 +0100

    Basic structure of the internal workings.

--- src/js/gagner.js
+++ src/js/gagner.js
@@ -3,6 +3,46 @@ var Gagner = (function() {
 
     'use strict';
 
-    return true;
+    var setup = {
+
+        buttons: false,
+
+        // Default configuration settings.
+        defaults: {
+            showResults: 1
+        },
+
+        init: function(elements, options) {
+
+            // Assign all of the elements to the object·
+            // @ TODO - determine if we have an array or just one element.
+
+            // Loop over all of the properties and update the defaults if valid.
+            for (var i in options) {
+                if (options.hasOwnProperty(i) && this.defaults.hasOwnProperty(options[i])) {
+                    this.defaults[i] = options[i];
+                }
+            }
+
+            this.bindEvents();
+            this.generateScoreboard();
+
+
+        },
+
+        bindEvents: function() {
+
+        },
+
+        generateScoreboard: function() {
+
+        }
+
+    };
+
+    return function(elements, options) {
+      setup.init(elements, options);
+    };
+
 
 })();
